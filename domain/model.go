1210e22d6cb54ab8ef5212f751cc3e3384108b76
tomocy <tomocy.dev@gmail.com>
2019-08-03 02:11:13 +0900

    add for tweet to be readable string

--- domain/model.go
+++ domain/model.go
@@ -1,12 +1,19 @@
 package domain
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Tweet struct {
 	ID        string
 	User      *User
 	Text      string
 	CreatedAt time.Time
+}
+
+func (t *Tweet) String() string {
+	return fmt.Sprintf("%s @%s\t%s\n\n\t%s\t\n\n", t.User.Name, t.User.ScreenName, t.CreatedAt, t.Text)
 }
 
 type User struct {
