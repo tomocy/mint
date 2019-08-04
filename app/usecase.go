6c4cb8fc272a41484d9931be1139353cff246918
tomocy <tomocy.dev@gmail.com>
2019-08-04 16:24:56 +0900

    add to generate new usecases for tweets

--- app/usecase.go
+++ app/usecase.go
@@ -1,7 +1,20 @@
 package app
 
-import "github.com/tomocy/mint/domain"
+import (
+	"github.com/tomocy/deverr"
+	"github.com/tomocy/mint/domain"
+)
+
+func NewTweetUsecase(repo domain.TweetRepository) *TweetUsecase {
+	return &TweetUsecase{
+		repo: repo,
+	}
+}
 
 type TweetUsecase struct {
 	repo domain.TweetRepository
+}
+
+func (u *TweetUsecase) Fetch() ([]*domain.Tweet, error) {
+	return nil, deverr.NotImplemented
 }
