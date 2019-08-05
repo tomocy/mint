d294d003a46011feb3cfaa7417d67e099160f479
tomocy <tomocy.dev@gmail.com>
2019-08-05 17:42:15 +0900

    add to create workspace

--- infra/infra.go
+++ infra/infra.go
@@ -5,6 +5,11 @@ import (
 	"path/filepath"
 )
 
+func createWorkspace() error {
+	dir := filepath.Join(os.Getenv("HOME"), ".mint")
+	return os.MkdirAll(dir, 0700)
+}
+
 func configFilename() string {
 	return filepath.Join(os.Getenv("HOME"), ".mint/config.json")
 }
