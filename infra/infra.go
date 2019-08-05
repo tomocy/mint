a371c099045ab932d2e1a9efbc312c1d23f1e86c
tomocy <tomocy.dev@gmail.com>
2019-08-05 17:48:55 +0900

    fix

--- infra/infra.go
+++ infra/infra.go
@@ -6,8 +6,25 @@ import (
 )
 
 func createWorkspace() error {
+	name := configFilename()
+	if _, err := os.Stat(name); err == nil {
+		return nil
+	}
+
 	dir := filepath.Join(os.Getenv("HOME"), ".mint")
-	return os.MkdirAll(dir, 0700)
+	if err := os.RemoveAll(dir); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return err
+	}
+
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+
+	return f.Close()
 }
 
 func configFilename() string {
