0e9bf096e0d3c48abdfb8af246da71aeaa1e2375
tomocy <tomocy.dev@gmail.com>
2019-08-05 11:56:59 +0900

    add for app to set itself up

--- cmd/mint/main.go
+++ cmd/mint/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "github.com/urfave/cli"
+
 func main() {}
 
-type app struct{}
+type app struct {
+	driver *cli.App
+}
+
+func (a *app) setUp() {
+	a.setBasic()
+	a.setCommands()
+}
+
+func (a *app) setBasic() {
+	a.driver.Name = "mint"
+}
+
+func (a *app) setCommands() {
+	a.driver.Commands = []cli.Command{}
+}
