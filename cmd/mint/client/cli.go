8fa824d63374b01c45ac6230b8ad468c58d5c087
tomocy <tomocy.dev@gmail.com>
2019-08-05 15:12:46 +0900

    add for cli client to fetch home tweets and show them

--- cmd/mint/client/cli.go
+++ cmd/mint/client/cli.go
@@ -1,3 +1,30 @@
 package client
 
+import (
+	"fmt"
+
+	"github.com/tomocy/mint/app"
+	"github.com/tomocy/mint/domain"
+	"github.com/tomocy/mint/infra"
+)
+
 type CLI struct{}
+
+func (c *CLI) FetchHomeTweets() error {
+	r := infra.NewTwitter()
+	u := app.NewTweetUsecase(r)
+	tweets, err := u.Fetch()
+	if err != nil {
+		return err
+	}
+
+	c.showTweets(tweets)
+
+	return nil
+}
+
+func (c *CLI) showTweets(tweets []*domain.Tweet) {
+	for _, t := range tweets {
+		fmt.Println(t)
+	}
+}
