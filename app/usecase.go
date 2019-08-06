caefad2b80212b28ff8af1a42b50a57c23f98472
tomocy <tomocy.dev@gmail.com>
2019-08-07 00:51:04 +0900

    add (*TweetUsecase).PoleHomeTweetss

--- app/usecase.go
+++ app/usecase.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"context"
+
 	"github.com/tomocy/mint/domain"
 )
 
@@ -12,6 +14,10 @@ func NewTweetUsecase(repo domain.TweetRepository) *TweetUsecase {
 
 type TweetUsecase struct {
 	repo domain.TweetRepository
+}
+
+func (u *TweetUsecase) PoleHomeTweets(ctx context.Context) (<-chan []*domain.Tweet, <-chan error) {
+	return u.repo.PoleHomeTweets(ctx)
 }
 
 func (u *TweetUsecase) FetchHomeTweets() ([]*domain.Tweet, error) {
