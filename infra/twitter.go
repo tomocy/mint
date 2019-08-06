8ebd4dfc00d4a8d3faa3f8fb6d323a5def742f49
tomocy <tomocy.dev@gmail.com>
2019-08-07 00:50:24 +0900

    add (*Twitter).PoleHomeTweets to pole home tweets in 5 minutes

--- infra/twitter.go
+++ infra/twitter.go
@@ -2,12 +2,14 @@ package infra
 
 import (
 	"bufio"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/garyburd/go-oauth/oauth"
 	"github.com/tomocy/mint/domain"
@@ -31,6 +33,48 @@ func NewTwitter() *Twitter {
 
 type Twitter struct {
 	oauthClient *oauth.Client
+}
+
+func (t *Twitter) PoleHomeTweets(ctx context.Context) (<-chan []*domain.Tweet, <-chan error) {
+	tsCh, errCh := make(chan []*domain.Tweet), make(chan error)
+	go func() {
+		defer func() {
+			close(tsCh)
+			close(errCh)
+		}()
+
+		sendTweets := func(lastID string, tsCh chan<- []*domain.Tweet, errCh chan<- error) string {
+			var params url.Values
+			if lastID != "" {
+				params = url.Values{
+					"since_id": []string{lastID},
+				}
+			}
+			ts, err := t.fetchTweets("https://api.twitter.com/1.1/statuses/home_timeline.json", params)
+			if err != nil {
+				errCh <- err
+				return ""
+			}
+			if len(ts) <= 0 {
+				return ""
+			}
+
+			tsCh <- ts
+			return ts[0].ID
+		}
+
+		lastID := sendTweets("", tsCh, errCh)
+		for {
+			select {
+			case <-ctx.Done():
+				break
+			case <-time.After(5 * time.Minute):
+				lastID = sendTweets(lastID, tsCh, errCh)
+			}
+		}
+	}()
+
+	return tsCh, errCh
 }
 
 func (t *Twitter) FetchHomeTweets() ([]*domain.Tweet, error) {
