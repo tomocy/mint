99d241ad63c7fa4d5f376df5a422ed9d47831ab2
tomocy <tomocy.dev@gmail.com>
2019-08-07 01:35:56 +0900

    add orderOlderTweets

--- cmd/mint/client/client.go
+++ cmd/mint/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/tomocy/mint/domain"
@@ -15,9 +16,19 @@ func (ts asciiTweets) String() string {
 		if i == 0 {
 			b.WriteString("----------\n")
 		}
-		b.WriteString(fmt.Sprintf("%s@%s %s\n%s\n", t.User.Name, t.User.ScreenName, t.CreatedAt.Format("2006/01/02 15:16"), t.Text))
+		b.WriteString(fmt.Sprintf("%s@%s %s\n%s\n", t.User.Name, t.User.ScreenName, t.CreatedAt.Format("2006/01/02 15:04"), t.Text))
 		b.WriteString("----------\n")
 	}
 
 	return b.String()
+}
+
+func orderOlderTweets(ts []*domain.Tweet) []*domain.Tweet {
+	ordered := make([]*domain.Tweet, len(ts))
+	copy(ordered, ts)
+	sort.Slice(ordered, func(i, j int) bool {
+		return ordered[i].CreatedAt.Before(ordered[j].CreatedAt)
+	})
+
+	return ordered
 }
